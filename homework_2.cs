775272b493bcc7331f4e7f9447590c818e325730
Ilya Gladkiy <steel.series.1337.el@gmail.com>
2022-08-11 00:10:50 +0300

    HW done

---
+++ homework_2.cs
@@ -0,0 +1,29 @@
+// Задача 10
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 100 || num > 999)
+// {
+//     Console.WriteLine("Введено не трехзначное число!"); 
+// }
+// else 
+// {
+//     Console.WriteLine($"Вторая цифра введенного числа: {(num / 10) % 10}");
+// }
+
+// Задача 13
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// char[] array = Math.Abs(num).ToString().ToCharArray();
+// Console.WriteLine(array.Length > 3 ? $"Третья цифра введенного числа: {array[2]}": "Третьей цифры нет");
+
+// Задача 15
+// Console.Write("Введите день недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Несуществующий день недели!");
+// }
+// else 
+// {
+//     Console.Write(day == 6 || day == 7? "Выходной день": "Рабочий день");
+// }
