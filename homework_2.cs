b03511cb529a39cc2f3d7d3dc0ae4d87c24daa84
Ilya Gladkiy <steel.series.1337.el@gmail.com>
2022-08-12 00:15:25 +0300

    hw_done

--- homework_2.cs
+++ homework_2.cs
@@ -27,3 +27,20 @@
 // {
 //     Console.Write(day == 6 || day == 7? "Выходной день": "Рабочий день");
 // }
+
+// Console.Write("Input random number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(Math.Abs(num) < 100)
+// { 
+//     Console.Write("There is no third digit");
+// }
+
+// else
+// {
+//     while(Math.Abs(num) >= 1000)
+//     {
+//         num = num/10;
+//     }
+//     Console.Write(Math.Abs(num = num%10));
+// }
